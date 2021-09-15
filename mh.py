103be2f36dc1293fa6b1f797239448b1d80c841e
huynhnhathao <18520714@gm.uit.edu.vn>
2021-09-15 08:33:46 +0700

    format code

--- mh.py
+++ mh.py
@@ -1,15 +1,21 @@
 import numpy as np
 
 class MontyGame:
-    def __init__(self):
+    """
+    Monty Hall game
+    """
+    
+    def __init__(self) -> None:
         self.door1 = False
         self.door2 = False
         self.door3 = False
 
         self.all_door = [self.door1,self.door2, self.door3]
         
-    def reset(self):
-
+    def reset(self) -> None:
+        """
+        Reset the game, randomly put a car behind a door
+        """
         self.player_choice = None
         self.monty_opened = None
         car_door = np.random.randint(1, 4, 1)[0]
@@ -17,7 +23,10 @@ class MontyGame:
             self.all_door[i] = False
         self.all_door[car_door - 1] = True
 
-    def choose_door(self,your_choice )-> int:
+    def choose_door(self,your_choice:int )-> int:
+        """
+        Player choosing a door in [1,2,3].
+        """
         # you choose a door, and monty open another door, 
         # and return you the door number that has a goat
         self.player_choice = your_choice
@@ -33,23 +42,20 @@ class MontyGame:
         for i, door in enumerate(self.all_door):
             if i + 1 != your_choice and not door:
                 self.monty_opened  = i+1
-
                 return self.monty_opened
-
         return -1
 
-    def final_result(self, your_choice: bool):
+    def final_result(self, your_choice: bool) -> bool:
+        """
+        Game result
+        """
         # you choose switch or not, and monty tell you win or not
         # he choose to switch
         if your_choice:
             return self.if_switch
-
         # he choose to keep
         if not your_choice:
             return self.if_not_switch
-
-        return None
-
 
 def play_game(num_games:int,strategy:str, game: MontyGame) -> float:
     # play num_games of monty game with strategy and return the fraction of winning
@@ -70,11 +76,9 @@ def play_game(num_games:int,strategy:str, game: MontyGame) -> float:
             result = game.final_result(False)
             if result:
                 num_win += 1
-
     return num_win/num_games
 
-
 if __name__ == '__main__':
-    
     game = MontyGame()
-    play_game(100000, 'not_switch', game)
+    print(play_game(100000, 'not_switch', game))
+    print(play_game(100000, 'switch', game))
